958fc5557a575717501cc180b67155b171d92054
Joe Beuckman <joebeuckman@gmail.com>
2017-04-05 23:08:20 -0500

    swagger-ui from cdn

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ var express = require('express');
 module.exports.addRoutes = function (app, keystone) {
     console.log('keystone-rest.addRoutes(app, keystone)');
 
-    console.log(keystone.lists);
-
-
     var swaggerJSDoc = require('swagger-jsdoc');
 
     var swaggerDefinition = {
         info: {
-            title: 'KeystoneJS REST API',
+            title: keystone._options.name+' REST API',
             version: '0.0.1',
             description: '',
         },
@@ -29,14 +26,48 @@ module.exports.addRoutes = function (app, keystone) {
     // initialize swagger-jsdoc
     var swaggerSpec = swaggerJSDoc(options);
 
-    swaggerSpec.paths["api/v1/cool"] = {
-        "get": {}
+    for (var key in keystone.lists) {
+        var resource = keystone.lists[key].path;
+                
+        swaggerSpec.paths['/'+resource] = {
+            "post": {
+                tags: [key]
+            },
+            "get": {
+                tags: [key]
+            },
+            "put": {
+                tags: [key]
+            },
+            "delete": {
+                tags: [key]
+            }
+        };
+
+        console.log(Object.keys(keystone.lists[key].fields));
+        
+        var definition = {
+            type: 'object',
+            properties: {
+                
+            }
+        };
+        
+        for (var property in keystone.lists[key].fields) {
+            definition.properties[property] = {
+                type: 'string'
+            }
+        }
+        
+        swaggerSpec.definitions[key] = definition;
+        
     }
 
     app.get('/swagger.json', function (req, res) {
         res.setHeader('Content-Type', 'application/json');
         res.send(swaggerSpec);
     });
-
+    
+    app.use('/api-docs', express.static(__dirname+'/html'));
 
 };
