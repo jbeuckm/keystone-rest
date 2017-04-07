27fe84cef4c276efa89fd1f41a83c355958c8647
joe <joseph.beuckman@usbank.com>
2017-04-07 12:10:09 -0500

    get route working

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 var express = require('express');
 
-module.exports.addRoutes = function (app, keystone) {
+module.exports.addRoutes = function (app, keystone, basePath) {
+
+    basePath = basePath || '/api/v1';
+
     console.log('keystone-rest.addRoutes(app, keystone)');
 
     var swaggerJSDoc = require('swagger-jsdoc');
 
     var swaggerDefinition = {
         info: {
-            title: keystone._options.name+' REST API',
+            title: keystone._options.name + ' REST API',
             version: '0.0.1',
             description: '',
         },
         host: 'localhost:3000',
-        basePath: '/',
+        basePath: basePath + '/',
     };
 
     // options for the swagger docs
@@ -22,32 +25,42 @@ module.exports.addRoutes = function (app, keystone) {
         // path to the API docs
         apis: ['./server.js', './models/*.js', './resources/*.js'],
     };
-    
-    
+
+
     var swagger_helper = require('./lib/swagger_helper');
     var route_helper = require('./lib/route_helper');
+
+    var apiRouter = require('express').Router();
+    apiRouter.use(function (req, res, next) {
+        req.keystone = keystone;
+        next();
+    });
+    route_helper.buildRoutes(apiRouter);
+
+    app.use(basePath, apiRouter);
+
 
     // initialize swagger-jsdoc
     var swaggerSpec = swaggerJSDoc(options);
 
     for (var key in keystone.lists) {
-        
+
         var list = keystone.lists[key];
-        
+
         var paths = swagger_helper.buildPaths(list);
         for (var path in paths) {
             swaggerSpec.paths[path] = paths[path];
         }
 
         swaggerSpec.definitions[key] = swagger_helper.buildDefinition(list);
-        
+
     }
 
     app.get('/swagger.json', function (req, res) {
         res.setHeader('Content-Type', 'application/json');
         res.send(swaggerSpec);
     });
-    
-    app.use('/api-docs', express.static(__dirname+'/html'));
+
+    app.use('/api-docs', express.static(__dirname + '/html'));
 
 };
